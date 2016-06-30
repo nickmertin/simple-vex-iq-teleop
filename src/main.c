b5e45e4535c62d9f08477956063919703d20c147
Nicholas Mertin <nmertin@mertin.home>
2016-06-30 10:01:39 -0400

    Added GPL header

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma config(Sensor, port7,  touch_led,      sensorVexIQ_LED)
 #pragma config(Motor,  motor1,          left_drive,    tmotorVexIQ, PIDControl, encoder)
 #pragma config(Motor,  motor2,          right_drive,   tmotorVexIQ, PIDControl, reversed, encoder)
